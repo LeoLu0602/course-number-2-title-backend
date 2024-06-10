303648114b64506c7a9950642ffae9b918b23c74
Kenny Cartman <lyuholu@gmail.com>
2024-06-10 18:48:21 +0800

    feat: connect express to PostgreSQL

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,45 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const supabase_js_1 = require("@supabase/supabase-js");
+require("dotenv/config");
 const PORT = 3001;
 const app = (0, express_1.default)();
+const supabase = (0, supabase_js_1.createClient)((_a = process.env.SUPABASE_URL) !== null && _a !== void 0 ? _a : '', (_b = process.env.SUPABASE_KEY) !== null && _b !== void 0 ? _b : '');
+app.get('/api/v1/courses', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { school, department, courseNumber } = req.query;
+    const courseTitle = yield getCourseTitle(school, department, courseNumber);
+    res.json({ courseTitle });
+}));
+function getCourseTitle(school, department, courseNumber) {
+    var _a, _b;
+    return __awaiter(this, void 0, void 0, function* () {
+        const { data, error } = yield supabase
+            .from('course')
+            .select('*')
+            .eq('school', school)
+            .eq('department', department)
+            .eq('course_number', courseNumber);
+        if (error) {
+            console.error('getCourseTitle Error:', error);
+            return '';
+        }
+        return (_b = (_a = data === null || data === void 0 ? void 0 : data[0]) === null || _a === void 0 ? void 0 : _a.course_title) !== null && _b !== void 0 ? _b : '';
+    });
+}
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,49 @@
 import express from 'express';
+import { PostgrestError, createClient } from '@supabase/supabase-js';
+import 'dotenv/config';
 
 const PORT: number = 3001;
 
 const app = express();
+
+const supabase = createClient(
+  process.env.SUPABASE_URL ?? '',
+  process.env.SUPABASE_KEY ?? ''
+);
+
+app.get('/api/v1/courses', async (req, res) => {
+  const { school, department, courseNumber } = req.query;
+
+  const courseTitle: string = await getCourseTitle(
+    school as string,
+    department as string,
+    courseNumber as string
+  );
+
+  res.json({ courseTitle });
+});
+
+async function getCourseTitle(
+  school: string,
+  department: string,
+  courseNumber: string
+): Promise<string> {
+  const { data, error }: { data: any[] | null; error: PostgrestError | null } =
+    await supabase
+      .from('course')
+      .select('*')
+      .eq('school', school)
+      .eq('department', department)
+      .eq('course_number', courseNumber);
+
+  if (error) {
+    console.error('getCourseTitle Error:', error);
+
+    return '';
+  }
+
+  return data?.[0]?.course_title ?? '';
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
